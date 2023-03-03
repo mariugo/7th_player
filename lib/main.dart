a4bbd3325d917b29fdd14d004157d7e2a693f651
Mariugo Megiani <mariugomegiani@gmail.com>
2023-03-03 20:51:46 -0300

    Material 3 Theme impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:seventh_player/app/theme/app_colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +11,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Seventh Player',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: darkColorScheme,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: null,
